059d197784df32ba75b8b34f6d4ca7db5558af92
Altusha <240782@astanait.edu.kz>
2025-09-18 13:09:42 +0600

    Check my CoffeeMaker which have to createDrink()

--- src/CoffeeMaker.java
+++ src/CoffeeMaker.java
@@ -2,3 +2,22 @@
 abstract class CoffeeMaker {
     public abstract Coffee createDrink();
 }
+
+class EspressoCoffeeMaker extends CoffeeMaker {
+    @Override
+    public Coffee createDrink() {
+        return new Espresso();
+    }
+}
+class LatteMaker  extends CoffeeMaker {
+    @Override
+    public Coffee createDrink() {
+        return new Latte();
+    }
+}
+class CappuccinoMaker extends CoffeeMaker {
+    @Override
+    public Coffee createDrink() {
+        return new Cappuccino();
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-
+        CoffeeMaker maker = new LatteMaker();
+        Coffee myDrink = maker.createDrink();
+        System.out.println(myDrink.name());
     }
 }
